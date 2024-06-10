183f634c8a9b13592b3d6e6b8fd6a4b499755ab2
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-10 11:17:14 +0430

    Articles and User component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import Articles from "./components/Articles";
+import User from "./components/User";
 
 function App() {
   return (
     <div className="App">
-     
+      <header>
+        <h1>React Skeletons</h1>
+      </header>
+      <div className="content">
+        <Articles />
+        <User />
+      </div>
     </div>
   );
 }
